c9e635d46362df5620b95d3d4f17af2aa054a0c3
jorge52an <jorgesolavila@gmail.com>
2017-04-03 10:16:43 -0500

    Feat: add libraries

--- src/app/js/mainController.js
+++ src/app/js/mainController.js
@@ -1,4 +1,8 @@
 angular.module( "RentApp" )
-.controller( "MainController", function( $scope )
+.controller( "MainController", function( $scope, NgMap )
 {
+	$scope.map = {
+		initialPosition: [41.8708, -87.6505],
+		zoom: 15
+	};
 } );
